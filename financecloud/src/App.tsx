98f73174f12c271b5d225fa0c0e6123ff6c4bf58
naneo10 <fenerte10@gmail.com>
2024-01-19 16:50:46 +0900

    20240119 pjh

--- financecloud/src/App.tsx
+++ financecloud/src/App.tsx
@@ -5,9 +5,14 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Home from './pages/a_home';
 import Login from './pages/b_login';
 import SignUp from './pages/b_signUp';
+import Notification from './pages/b_notification';
+import NotificationDetails from './pages/b_notificationDetails';
 
 import Customer from './pages/c_customer';
+
 import Seller from './pages/g_seller';
+import SellerApplication from './pages/g_sellerApplication';
+import SellerManagement from './pages/g_sellerManagement';
 
 import FundingMain from './pages/d_fundingMain';
 import FundingView from './pages/e_fundingView';
@@ -24,9 +29,14 @@ function App() {
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/login'>Login</Link></li>
           <li><Link to='/signUp'>SignUp</Link></li>
+          <li><Link to='/notification'>Notification</Link></li>
+          <li><Link to='/notificationDetails'>NotificationDetails</Link></li>
 
           <li><Link to='/customer'>Customer</Link></li>
+
           <li><Link to='/seller'>Seller</Link></li>
+          <li><Link to='/sellerApplication'>SellerApplication</Link></li>
+          <li><Link to='/sellerManagement'>SellerManagement</Link></li>
 
           <li><Link to='/fundingMain'>FundingMain</Link></li>
           <li><Link to='/fundingView'>FundingView</Link></li>
@@ -40,9 +50,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signUp' element={<SignUp />} />
+        <Route path='/notification' element={<Notification />} />
+        <Route path='/notificationDetails' element={<NotificationDetails />} />
 
         <Route path="/customer" element={<Customer />} />
+
         <Route path="/seller" element={<Seller />} />
+        <Route path="/sellerApplication" element={<SellerApplication />} />
+        <Route path="/sellerManagement" element={<SellerManagement />} />
 
         <Route path="/fundingMain" element={<FundingMain />} />
         <Route path="/fundingView" element={<FundingView />} />
--- financecloud/src/pages/b_signUp/index.tsx
+++ financecloud/src/pages/b_signUp/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+//!회원가입
+//?내용
+//아이디, 비밀번호, 이메일 입력 후 회원가입 절차 완료
+
 export default function Index() {
   return(
     <>
