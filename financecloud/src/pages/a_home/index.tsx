e1eb3fbb9af17067bb08838c820743f5ce2eb78f
naneo10 <fenerte10@gmail.com>
2024-01-19 15:07:13 +0900

    20240119 pjh

---
+++ financecloud/src/pages/a_home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//!메인페이지
+//?기능
+//로그인, 회원가입 - 팝업창으로 가입 진행
+//소비자, 판매자 페이지 이동
+
+//?내용
+//-크라우드 펀딩 구조
+//-
+export default function Index() {
+  return(
+    <>
+    </>
+  )
+}
